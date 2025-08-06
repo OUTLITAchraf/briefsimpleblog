8cc5ffca3b5c0d308804b47628972586d06ae8e6
OUTLITAchraf <outlitachraf5@gmail.com>
2025-08-06 22:43:56 +0100

    cree un article sans envoyer à la page list articles

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ArticleList from "./Simpleblog Pages/ArticleList";
 import Logo from "./Simpleblog Pages/asset/Logo_SimpleBlog.png";
 import ArticleDetail from "./Simpleblog Pages/ArticleDetail";
+import NewArticleForm from "./Simpleblog Pages/NewArticleForm";
 function App() {
   return (
     <>
     <Router>
       <nav className="flex justify-between px-20 bg-[#F8F9FA]">
+        <a href="/">
         <img src={Logo} alt="logo" width={"100px"} />
+        </a>
         <button className="bg-[#1E293B] text-white py-2 px-3 my-6 rounded-xl">
-          Create Post
+          <a href="/create_article">Create Post</a>
         </button>
       </nav>
       <main className="mx-20 mt-10">
         <Routes>
           <Route path="/" element={<ArticleList />}/>
           <Route path="/details/:index" element={<ArticleDetail />}/>
+          <Route path="/create_article" element={<NewArticleForm />}/>
         </Routes>
       </main>
       <footer className="flex justify-between items-center px-10 mt-20 bg-[#F8F9FA]">
