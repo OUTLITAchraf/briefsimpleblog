3439614adb26409ba120bd891465a2d549a009e6
OUTLITAchraf <outlitachraf5@gmail.com>
2025-08-06 11:45:45 +0100

    install react route et ajoute les route en App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ArticleList from "./Simpleblog Pages/ArticleList";
 import Logo from "./Simpleblog Pages/asset/Logo_SimpleBlog.png";
+import ArticleDetail from "./Simpleblog Pages/ArticleDetail";
 function App() {
   return (
     <>
+    <Router>
       <nav className="flex justify-between px-20 bg-[#F8F9FA]">
         <img src={Logo} alt="logo" width={"100px"} />
         <button className="bg-[#1E293B] text-white py-2 px-3 my-6 rounded-xl">
@@ -10,7 +13,10 @@ function App() {
         </button>
       </nav>
       <main className="mx-20 mt-10">
-        <ArticleList />
+        <Routes>
+          <Route path="/" element={<ArticleList />}/>
+          <Route path="/details/:index" element={<ArticleDetail />}/>
+        </Routes>
       </main>
       <footer className="flex justify-between items-center px-10 mt-20 bg-[#F8F9FA]">
         <img src={Logo} alt="logo" width={"200px"} />
@@ -20,6 +26,7 @@ function App() {
           <p>Privacy policy</p>
         </div>
       </footer>
+      </Router>
     </>
   );
 }
