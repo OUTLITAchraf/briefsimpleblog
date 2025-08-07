2c17eefc46c19d4740a2336936d5220a03961b15
OutlitAchraf <achrafking121@gmail.com>
2025-08-07 12:18:37 +0100

    ajoute an article et envoyer à page article list avec message de succes

--- src/Simpleblog Pages/ArticleDetail.jsx
+++ src/Simpleblog Pages/ArticleDetail.jsx
@@ -11,26 +11,29 @@ export default function DetailsArticle() {
         window.scrollTo({ top: 0, behavior: "smooth" });
     }, []);
 
-    if (!article) return <p className="text-red-500">No article data found.</p>;
+    if (!article) return <p className="text-red-500 text-3xl">No article data found.</p>;
 
     return (
         <div className="px-10 py-5">
             <button onClick={() => navigate("/")}
                 className="bg-[#1E293B] text-white py-2 pl-1 pr-3 rounded-md mb-10 flex "
-            ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6" /></svg>
-                Back</button>
+            ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6" /></svg>
+                Back
+            </button>
             <h1 className="text-4xl font-bold mb-4">{article.title}</h1>
-            <img src={article.urlToImage} alt="Article" className="w-full max-h-[500px] object-cover mb-5" />
+            <img src={article.urlToImage} alt="Article" className="w-full max-h-full object-cover mb-5" />
             <div className="flex flex-row justify-between">
                 <p className="text-gray-500 text-md mb-2">
                     By {article.author || "Unknown"}
                 </p>
                 <p className="flex flex-row items-center text-gray-500 text-md mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-icon lucide-calendar mr-2"><path d="M8 2v4" /><path d="M16 2v4" /><rect width="18" height="18" x="3" y="4" rx="2" /><path d="M3 10h18" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar-icon lucide-calendar mr-2"><path d="M8 2v4" /><path d="M16 2v4" /><rect width="18" height="18" x="3" y="4" rx="2" /><path d="M3 10h18" /></svg>
                     {article.publishedAt ? article.publishedAt.replace('T',' | ').replace('Z',''):""}
                 </p>
             </div>
-
+            <h3 className="text-2xl font-semibold my-3 ">Description</h3>
+            <p className="text-lg text-gray-700">{article.description}</p>
+            <h3 className="text-2xl font-semibold my-3 ">Content</h3>
             <p className="text-lg text-gray-700">{article.content}</p>
         </div>
     );
