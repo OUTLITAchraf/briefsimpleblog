b2af1a416ad073b51b337b307cfe25943334c4aa
OutlitAchraf <achrafking121@gmail.com>
2025-08-08 11:24:12 +0100

    add code responsive of phone in pages App.js and ArticleList.jsx and ArticleCarsouel.jsx

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ function App() {
   return (
     <>
     <Router>
-      <nav className="flex justify-between px-20 bg-[#F8F9FA]">
+      <nav className="flex justify-between px-5 lg:px-20 bg-[#F8F9FA]">
         <a href="/">
-        <img src={Logo} alt="logo" width={"100px"} />
+        <img src={Logo} alt="logo" className="w-[70px] lg:w-[100px]" />
         </a>
-        <button className="bg-[#1E293B] text-white py-2 px-3 my-6 rounded-xl">
+        <button className="bg-[#1E293B] text-white text-xs px-2 py-2 rounded-md lg:text-base lg:py-2 lg:px-3 my-6 lg:rounded-xl">
           <a href="/create_article">Create Article</a>
         </button>
       </nav>
-      <main className="mx-20 mt-10">
+      <main className="lg:mx-20 mt-10">
         <Routes>
           <Route path="/" element={<ArticleList />}/>
           <Route path="/details/:index" element={<ArticleDetail />}/>
           <Route path="/create_article" element={<NewArticleForm />}/>
         </Routes>
       </main>
-      <footer className="flex justify-between items-center px-10 mt-20 bg-[#F8F9FA]">
-        <img src={Logo} alt="logo" width={"200px"} />
-        <p className="text-gray-500 mt-10">&copy; 2025 SimpleBlog.</p>
-        <div className="text-gray-500 mr-10">
+      <footer className="flex justify-between items-center mt-10 lg:px-10 lg:mt-20 bg-[#F8F9FA]">
+        <img src={Logo} alt="logo" className="w-[100px] lg:w-[200px]" />
+        <p className="text-gray-500 text-xs lg:text-xl mt-10">&copy; 2025 SimpleBlog.</p>
+        <div className="text-gray-500 text-xs mr-5 lg:text-xl lg:mr-10">
           <p>Terms of us</p>
           <p>Privacy policy</p>
         </div>
