dbafc3e28f4f196b4da0b093319f57d102ec11f7
OutlitAchraf <achrafking121@gmail.com>
2025-08-08 15:14:41 +0100

    add validation of form

--- src/Simpleblog Pages/NewArticleForm.jsx
+++ src/Simpleblog Pages/NewArticleForm.jsx
@@ -8,6 +8,7 @@ export default function NewArticleForm() {
     const [date_public, setDate_public] = useState('');
     const [content, setContent] = useState('');
     const [image, setImage] = useState(null);
+    const [errors, setErrors] = useState({});
     const navigate = useNavigate();
 
     function handleImageChange(e) {
@@ -22,7 +23,47 @@ export default function NewArticleForm() {
     }
 
     function AddArticle(e) {
+
         e.preventDefault();
+
+        let formIsValid = true;
+        let newErrors = {};
+
+        if (!author) {
+            formIsValid = false;
+            newErrors.author = 'Veuillez remplir le champ de author obligatoire.';
+        }
+
+        if (!title) {
+            formIsValid = false;
+            newErrors.title = 'Veuillez remplir le champ de title obligatoire.';
+        }
+
+        if (!description) {
+            formIsValid = false;
+            newErrors.description = 'Veuillez remplir le champ de description obligatoire.';
+        }
+
+        if (!date_public) {
+            formIsValid = false;
+            newErrors.date_public = 'Veuillez remplir le champ de date publishe obligatoire.';
+        }
+
+        if (!content) {
+            formIsValid = false;
+            newErrors.content = 'Veuillez remplir le champ de content obligatoire.';
+        }
+
+        if (!image) {
+            formIsValid = false;
+            newErrors.image = 'Veuillez remplir le champ de image obligatoire.';
+        }
+
+        if (!formIsValid) {
+            setErrors(newErrors);
+            return;
+        }
+
         const newArticle = {
             id: Date.now(),
             author: author,
@@ -32,6 +73,7 @@ export default function NewArticleForm() {
             content: content,
             urlToImage: image
         }
+
 
         const existingArticles = JSON.parse(localStorage.getItem("userArticles")) || [];
         const updatedArticles = [newArticle, ...existingArticles];
@@ -43,6 +85,7 @@ export default function NewArticleForm() {
         setDate_public('');
         setContent('');
         setImage(null);
+        setErrors({});
 
         navigate("/", { state: { success: true } });
     }
@@ -68,27 +111,33 @@ export default function NewArticleForm() {
             <form className="mx-10 my-5" onSubmit={AddArticle}>
                 <div className="mb-5">
                     <label htmlFor="author" className="block mb-2 text-sm font-medium text-gray-900">Author</label>
-                    <input type="text" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={author} onChange={handlerAuthor} required />
+                    <input type="text" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={author} onChange={handlerAuthor} />
+                    {errors.author && <p className="text-red-500 text-sm mt-1">{errors.author}</p>}
                 </div>
                 <div className="mb-5">
                     <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
-                    <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={title} onChange={handlerTitle} required />
+                    <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={title} onChange={handlerTitle} />
+                    {errors.title && <p className="text-red-500 text-sm mt-1">{errors.title}</p>}
                 </div>
                 <div className="mb-5">
                     <label htmlFor="desription" className="block mb-2 text-sm font-medium text-gray-900">Desription</label>
-                    <input type="text" id="desription" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={description} onChange={handlerDescription} required />
+                    <input type="text" id="desription" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={description} onChange={handlerDescription} />
+                    {errors.desription && <p className="text-red-500 text-sm mt-1">{errors.desription}</p>}
                 </div>
                 <div className="mb-5">
                     <label htmlFor="date_public" className="block mb-2 text-sm font-medium text-gray-900">Date Public</label>
-                    <input type="date" id="date_public" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={date_public} onChange={handlerDate_public} required />
+                    <input type="date" id="date_public" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={date_public} onChange={handlerDate_public} />
+                    {errors.date_public && <p className="text-red-500 text-sm mt-1">{errors.date_public}</p>}
                 </div>
                 <div className="mb-5">
                     <label htmlFor="content" className="block mb-2 text-sm font-medium text-gray-900">Content</label>
-                    <textarea id="content" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" value={content} onChange={handlerContent} required></textarea>
+                    <textarea id="content" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" value={content} onChange={handlerContent}></textarea>
+                    {errors.content && <p className="text-red-500 text-sm mt-1">{errors.content}</p>}
                 </div>
                 <div className="mb-5">
                     <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900">Image</label>
-                    <input type="file" id="image" accept="image/*" onChange={handleImageChange} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" aria-describedby="user_avatar_help" required/>
+                    <input type="file" id="image" accept="image/*" onChange={handleImageChange} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" aria-describedby="user_avatar_help" />
+                    {errors.image && <p className="text-red-500 text-sm mt-1">{errors.image}</p>}
                 </div>
                 <button type="submit"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
